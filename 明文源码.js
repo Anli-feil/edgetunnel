e3952c607a155060cee3e1275c78b78a5b1ec2e2
Anli-feil <c2584818650@gmail.com>
2025-03-18 19:06:11 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -4,9 +4,9 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'd903a2f6-2c79-4a1c-abca-9dd1b007e7b2';
 
-let proxyIP = 'cdn.xn--b6gac.eu.org'; // workers.cloudflare.cyou bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org
+let proxyIP = 'cdn-all.xn--b6gac.eu.org'; // workers.cloudflare.cyou bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org
 
 if (!isValidUUID(userID)) {
 	throw new Error('uuid is not valid');
